cd516bf46d05ec14839a63ad245c28b915ffc2cc
novitasalsabila <novnovv10@gmail.com>
2024-10-17 20:32:21 +0700

    Fungsi ini menentukan UI aplikasi dengan Jetpack Compose.

--- app/src/main/java/com/example/p2basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2basiccompose/MainActivity.kt
@@ -34,3 +34,4 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        setContent {
