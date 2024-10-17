d3efc40a152a0a9f3d0cc4a2cc82027169adc175
novitasalsabila <novnovv10@gmail.com>
2024-10-17 20:46:20 +0700

    Column: Berfungsi untuk menyusun elemen UI secara vertikal.

--- app/src/main/java/com/example/p2basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2basiccompose/MainActivity.kt
@@ -48,3 +48,9 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
+fun BasicLayout(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
