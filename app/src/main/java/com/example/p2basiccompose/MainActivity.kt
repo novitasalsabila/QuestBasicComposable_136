e75205bc26c7198bc3ef799564276daaaf50f62f
novitasalsabila <novnovv10@gmail.com>
2024-10-17 20:24:06 +0700

    import dari berbagai library dan fungsi yang digunakan dalam kode

--- app/src/main/java/com/example/p2basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2basiccompose/MainActivity.kt
@@ -1,1 +1,30 @@
 package com.example.p2basiccompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.p2basiccompose.ui.theme.P2BasicComposeTheme
+import com.example.p2basiccompose.BasicLayout as BasicLayout
