a9c76abb4065561948c9b4a04ebc475684fc0bdc
novitasalsabila <novnovv10@gmail.com>
2024-10-17 20:19:23 +0700

    basiccompose

--- app/src/main/java/com/example/p2basiccompose/MainActivity.kt
+++ app/src/main/java/com/example/p2basiccompose/MainActivity.kt
@@ -1,2 +1,86 @@
 package com.example.p2basiccompose
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.p2basiccompose.ui.theme.P2BasicComposeTheme
+import com.example.p2basiccompose.BasicLayout as BasicLayout
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            P2BasicComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    BasicLayout(modifier = Modifier.fillMaxSize())
+                    BasicLayout(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun BasicLayout(modifier: Modifier = Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(text = "Login", style = TextStyle(fontSize = 50.sp,
+            fontWeight = FontWeight.Bold )
+        )
+        Text(text = "Ini adalah halaman login")
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = null,
+            modifier = Modifier.padding(top = 20.dp).size(200.dp)
+        )
+
+        Text(text = "Nama",
+            modifier = Modifier.padding(top=20.dp))
+        Text(text = "Novita Salsabila", color = Color.Red,
+            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold) )
+        Text(text = "20220140136",
+            style = TextStyle(fontSize = 30.sp, fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top=20.dp))
+        Image(painter = painterResource(id = R.drawable.novi), contentDescription = null,
+            modifier = Modifier.padding(top = 40.dp).size(300.dp))
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    P2BasicComposeTheme {
+        BasicLayout()
+    }
+}
